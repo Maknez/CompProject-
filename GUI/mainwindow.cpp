78e5f140fc4949652b203ddcea1de6c38b22d600
Maknez <themaknez@gmail.com>
2017-11-16 10:29:35 +0100

    Testowy commit

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <stdlib.h>
-
+//przykladowy commit
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
