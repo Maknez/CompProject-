c1be92b7db3592bebc63e82e7ec5db8f1732b39e
Paryszek <michal.parysz@yahoo.com>
2017-11-28 12:22:51 +0100

    GUI mozliwosc podmiany  przypisan dziwiekow pod przyciski

---
+++ ProjectQT/globals.cpp
@@ -0,0 +1,8 @@
+#include "globals.h"
+#include <stdlib.h>
+#include <strings.h>
+Globals::Globals()
+{
+    public:
+    String button1 = "beep.wav";
+}
---
+++ ProjectQT/globals.h
@@ -0,0 +1,12 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#include <stdlib.h>
+#include <strings.h>
+class Globals
+{
+public:
+    Globals();
+    String button1;
+};
+
+#endif // GLOBALS_H
